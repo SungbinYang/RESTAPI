748a46d3f6d031e815d85f2fec453a4501633ac1
SungbinYang <ysb05222@naver.com>
2022-02-14 22:01:22 +0900

    예외 테스트

--- demo-rest-api/src/test/java/me/sungbin/demo/accounts/AccountServiceTest.java
+++ demo-rest-api/src/test/java/me/sungbin/demo/accounts/AccountServiceTest.java
@@ -1,18 +1,23 @@
 package me.sungbin.demo.accounts;
 
 import me.sungbin.demo.common.TestDescription;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * packageName : me.sungbin.demo.accounts
@@ -37,6 +42,9 @@ public class AccountServiceTest {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
     @TestDescription("아이디를 찾는 테스트")
     public void findByUsername() {
@@ -58,5 +66,18 @@ public class AccountServiceTest {
 
         // Then
         assertEquals(password, userDetails.getPassword());
+    }
+
+    @Test
+    @TestDescription("아이디를 불러오다가 실패")
+    public void findByUsernameFail() {
+        String username = "random@email.com";
+
+        // Expected :: 이 부분을 먼저 예측을 해야한다.
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        this.accountService.loadUserByUsername(username);
     }
 }
