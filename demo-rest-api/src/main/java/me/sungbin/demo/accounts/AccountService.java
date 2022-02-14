d4ee289c18af4e7427b8fa27af247058c4f05b56
SungbinYang <ysb05222@naver.com>
2022-02-14 22:41:59 +0900

    스프링 시큐리티 폼 인증 설정

--- demo-rest-api/src/main/java/me/sungbin/demo/accounts/AccountService.java
+++ demo-rest-api/src/main/java/me/sungbin/demo/accounts/AccountService.java
@@ -7,10 +7,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -32,10 +32,17 @@ public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
         return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+    }
+
+    public Account saveAccount(Account account) {
+        account.setPassword(this.passwordEncoder.encode(account.getPassword()));
+        return this.accountRepository.save(account);
     }
 
     private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
--- demo-rest-api/src/test/java/me/sungbin/demo/accounts/AccountServiceTest.java
+++ demo-rest-api/src/test/java/me/sungbin/demo/accounts/AccountServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -40,7 +41,7 @@ public class AccountServiceTest {
     private AccountService accountService;
 
     @Autowired
-    private AccountRepository accountRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -58,14 +59,14 @@ public class AccountServiceTest {
                 .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
                 .build();
 
-        this.accountRepository.save(account);
+        this.accountService.saveAccount(account);
 
         // When
         UserDetailsService userDetailsService = accountService;
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
         // Then
-        assertEquals(password, userDetails.getPassword());
+        assertTrue(this.passwordEncoder.matches(password, userDetails.getPassword()));
     }
 
     @Test
