34577c619032dad9e8d0ff10b999e171729291f6
SungbinYang <ysb05222@naver.com>
2022-02-14 23:11:22 +0900

    스프링 시큐리티 OAuth2 인증서버 설정

--- demo-rest-api/src/main/java/me/sungbin/demo/config/AppConfig.java
+++ demo-rest-api/src/main/java/me/sungbin/demo/config/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -40,6 +41,7 @@ public class AppConfig {
     }
 
     @Bean
+    @Profile("prod")
     public ApplicationRunner applicationRunner() {
         return new ApplicationRunner() {
 
---
+++ demo-rest-api/src/test/java/me/sungbin/demo/config/AuthServerConfigTest.java
@@ -0,0 +1,64 @@
+package me.sungbin.demo.config;
+
+import me.sungbin.demo.accounts.Account;
+import me.sungbin.demo.accounts.AccountRole;
+import me.sungbin.demo.accounts.AccountService;
+import me.sungbin.demo.common.BaseControllerTest;
+import me.sungbin.demo.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * packageName : me.sungbin.demo.config
+ * fileName : AuthServerConfigTest
+ * author : rovert
+ * date : 2022/02/14
+ * description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2022/02/14       rovert         최초 생성
+ */
+
+public class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @TestDescription("인증 토큰을 발급 받는 테스트")
+    public void getAuthToken() throws Exception {
+        //Given
+        String username = "sungbin@email.com";
+        String password = "sungbin";
+
+        Account sungbin = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+
+        this.accountService.saveAccount(sungbin);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                        .with(httpBasic(clientId, clientSecret))
+                        .param("username", username)
+                        .param("password", password)
+                        .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+    }
+}
